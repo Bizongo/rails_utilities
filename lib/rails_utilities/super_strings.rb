5e0ddac180d866a0cda3a2f30815cb2a50f7d9ed
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2018-01-02 13:27:41 +0530

    Added methods to super charge strings and objects

---
+++ lib/rails_utilities/super_strings.rb
@@ -0,0 +1,17 @@
+class String
+  def is_i?
+    /\A[-+]?\d+\z/ === self
+  end
+
+  def is_email?
+    /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i === self
+  end
+
+  def prepend_a_or_an
+    %w(a e i o u).include?(self[0].downcase) ? "an #{self}" : "a #{self}"
+  end
+
+  def strip_country_code(country_code = "+91")
+    self.split(country_code).last
+  end
+end
