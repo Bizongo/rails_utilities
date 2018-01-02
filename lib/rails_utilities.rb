0af4f7be7a4abb172064ff3a2d52869f03da4878
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2018-01-02 15:50:54 +0530

    Added contact number validator

--- lib/rails_utilities.rb
+++ lib/rails_utilities.rb
@@ -1,6 +1,7 @@
 require "rails_utilities/version"
 require "rails_utilities/super_strings"
 require "rails_utilities/super_objects"
+require "rails_utilities/contact_number_validator"
 
 # module RailsUtilities
 #   # Your code goes here...
