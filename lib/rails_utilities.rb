4c8596cb17043daf23853bc96af80600d26950f8
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2018-01-02 13:28:22 +0530

    Set up gem info. Added super objects and super strings

--- lib/rails_utilities.rb
+++ lib/rails_utilities.rb
@@ -1,5 +1,7 @@
 require "rails_utilities/version"
+require "rails_utilities/super_strings"
+require "rails_utilities/super_objects"
 
-module RailsUtilities
-  # Your code goes here...
-end
+# module RailsUtilities
+#   # Your code goes here...
+# end
